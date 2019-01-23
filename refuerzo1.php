116aa0bbe07933a755df051c156632a9039c4907
ferrandis24 <45199162+ferrandis24@users.noreply.github.com>
2019-01-23 11:47:21 +0100

    Add files via upload

---
+++ refuerzo1.php
@@ -0,0 +1,19 @@
+<?php
+$num=1;
+echo "<ul>";
+for ($i=0; $i < 20; $i++) {
+  echo "<li>$num</li>";
+  $num=$num+2;
+}
+echo "</ul>";
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+
+   </body>
+ </html>
